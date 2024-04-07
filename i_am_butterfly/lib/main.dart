cfcacd4eee8f7e282a3042685ee349fe5a724faf
zahra <shabrinazahra08@gmail.com>
2024-04-07 21:08:09 +0700

    pesan commit

--- i_am_butterfly/lib/main.dart
+++ i_am_butterfly/lib/main.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,30 +23,3 @@ void main() {
     ),
   );
 }
-=======
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'I am Butterfly',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          backgroundColor: Color.fromARGB(255, 96, 83, 117),
-        ),
-        body: Center(
-          child: Image(
-              image: AssetImage(
-                  'Images/Butterfly.png')),
-        ),
-        backgroundColor: Color.fromARGB(255, 177, 159, 211),
-      ),
-    ),
-  );
-}
->>>>>>> origin/master
