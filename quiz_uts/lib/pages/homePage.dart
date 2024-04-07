5cecaf02c4be64cc7206f3f9e587a5b709d36912
AmandaKhoiromaulSoviyanti <124539993+AmandaKhoiromaulSoviyanti@users.noreply.github.com>
2024-04-08 04:59:07 +0700

    Update homePage.dart

--- quiz_uts/lib/pages/homePage.dart
+++ quiz_uts/lib/pages/homePage.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:quiz_app/constants.dart';
-import 'package:quiz_app/pages/quizPage.dart';
+import 'package:flutter/material.dart';// Package untuk pengembangan UI dengan Flutter
+import 'package:get/get.dart';// Package untuk manajemen state dan navigasi
+import 'package:quiz_app/constants.dart';// File yang berisi konstanta-konstanta yang digunakan
+import 'package:quiz_app/pages/quizPage.dart';// Halaman kuis
 // import 'package:websafe_svg/websafe_svg.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({super.key});// Konstruktor kelas HomePage
 
   @override
   Widget build(BuildContext context) {
@@ -14,50 +14,50 @@ class HomePage extends StatelessWidget {
         children: [
           SafeArea(
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),// Padding horizontal dengan nilai default
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Spacer(flex: 8,),
+                  Spacer(flex: 8,),// Spacer untuk memberi ruang kosong di antara elemen dengan fleksibilitas 8
                   Text(
-                    "Quiziz Unesa",
+                    "Quiziz Unesa",// Judul halaman utama
                     style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.bold),
+                      color: Colors.white, fontWeight: FontWeight.bold),// Gaya teks untuk judul
                     ),
-                    Spacer(),
-                    Text("Masukkan Nama"),
-                    SizedBox(height: 12,),
-                    TextField(
+                    Spacer(),//memberi ruang kosong
+                    Text("Masukkan Nama"),// Label "Masukkan Nama"
+                    SizedBox(height: 12,),// Spasi vertikal
+                    TextField(// Widget untuk memasukkan nama
                       decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color(0xFF1C2341),
-                        hintText: "Nama",
+                        filled: true,// Mengisi latar belakang
+                        fillColor: Color(0xFF1C2341),// Warna latar belakang
+                        hintText: "Nama",// teks nama
                         border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                          borderRadius: BorderRadius.all(Radius.circular(12)),// Membuat sudut border melengkung
                         )
                       ),
                     ),
                     Spacer(),
-                    InkWell(
-                      onTap: () => Get.to(QuizPage()),
+                    InkWell(//Widget untuk memulai kuis
+                      onTap: () => Get.to(QuizPage()),// Aksi saat ditekan untuk navigasi ke halaman kuis
                       child: Container(
-                        width: double.infinity,
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                        width: double.infinity,// Lebar container menyesuaikan dengan lebar layar
+                        alignment: Alignment.center,// Posisi teks di tengah
+                        padding: EdgeInsets.all(kDefaultPadding * 0.75),// Padding untuk konten dalam container
                         decoration: BoxDecoration(
-                          gradient: kPrimaryGradient,
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                          gradient: kPrimaryGradient,// Gradasi warna untuk latar belakang
+                          borderRadius: BorderRadius.all(Radius.circular(12)),// Membuat sudut border melengkung
                         ),
                         child: Text(
-                          "Start Quiz",
+                          "Start Quiz",// Teks untuk tombol memulai kuis
                           style: Theme.of(context)
                             .textTheme
                             .button
-                            ?.copyWith(color: Colors.black),
+                            ?.copyWith(color: Colors.black),// Gaya teks untuk tombol
                         ),
                       ),
                     ),
-                    Spacer(flex: 8,),
+                    Spacer(flex: 8,),// Spacer untuk memberi ruang kosong di bagian bawah dengan fleksibilitas 8
                 ],
               ),
             ),
