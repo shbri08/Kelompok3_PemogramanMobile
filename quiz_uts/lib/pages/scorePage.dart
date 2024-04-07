318e2e3ba67f077302e837c81f3a8aba2a547b63
066_ShabrinaZahraAulia <117991094+shbri08@users.noreply.github.com>
2024-04-07 21:44:45 +0700

    Update scorePage.dart

--- quiz_uts/lib/pages/scorePage.dart
+++ quiz_uts/lib/pages/scorePage.dart
@@ -1,19 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:quiz_app/constants.dart';
-import 'package:quiz_app/controllers/quizController.dart';
+import 'package:flutter/material.dart'; 
+import 'package:get/get.dart';//import get untuk manajemen state dan navigasi 
+import 'package:quiz_app/constants.dart';//import file constants.dart
+import 'package:quiz_app/controllers/quizController.dart';//import controller QuizControler
 
+//widget untuk menamppilkan halaman scorr
 class ScorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //inisialisasi instance dari QuizController
     QuizController _qzController = Get.put(QuizController());
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: [
           Column(
+            //menyusun widget secara vertikal
             children: [
               Spacer(flex: 5),
+              //Text judul skor dengan gaya yang telah ditentukan 
               Text(
                 "Score",
                 style: Theme.of(context)
@@ -22,6 +26,7 @@ class ScorePage extends StatelessWidget {
                     ?.copyWith(color: kSecondaryColor),
               ),
               Spacer(),
+              // Tesk untuk menampilkan skor kuis
               Text(
                 "${_qzController.numOfCorrectAns * 10}/${_qzController.questions.length * 10}",
                 style: Theme.of(context)
@@ -29,7 +34,7 @@ class ScorePage extends StatelessWidget {
                     .headline4
                     ?.copyWith(color: kSecondaryColor),
               ),
-              Spacer(flex: 5),
+              Spacer(flex: 5),//untuk memberikan ruang kosong
             ],
           )
         ],
